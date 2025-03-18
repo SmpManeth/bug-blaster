c5427d318a918a29db9b87cfa0fa4c9760ad8e5a
smpmaneth <smpmaneth@gmail.com>
2025-03-19 01:05:25 +0530

    update App.js and ticketReducer.js

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -1,3 +1,8 @@
-export default function ticketReducer(state , action){
-
+export default function ticketReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TICKET":
+      return {...state , tickets: [...state.tickets , action.payload]}
+    default:
+      return state;
+  }
 }
