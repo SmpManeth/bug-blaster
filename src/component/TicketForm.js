9d7faa65e89995f1b89aca24028ea6673228b8b8
smpmaneth <smpmaneth@gmail.com>
2025-03-19 00:37:37 +0530

    update TicketForm.js

--- src/component/TicketForm.js
+++ src/component/TicketForm.js
@@ -46,9 +46,24 @@ export default function TicketForm() {
         />
       </div>
 
-        
+      <fieldset className="priority-fieldset">
+        <legend>Priority</legend>
 
+        {Object.entries(priorityLabels).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            />
+            {label}
+          </label>
+        ))}
+      </fieldset>
 
+        <button type="submit" className="button">  Submit</button>
     </form>
   );
 }
