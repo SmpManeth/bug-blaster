c462cdf19ab7c980463c47fcd63f80507e89fec5
smpmaneth <smpmaneth@gmail.com>
2025-03-19 01:29:17 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import TicketForm from "./component/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
 import { useReducer } from "react";
+import TicketList from "./component/TicketList";
 
 function App() {
 
@@ -15,6 +16,16 @@ function App() {
       <div className="container">
         <h1>Bug Blaster</h1>
         <TicketForm dispatch={dispatch} />
+
+        <h2>All Tickets</h2>
+        {
+          state.tickets.length > 0 ? (
+            <TicketList tickets={state.tickets} dispatch={dispatch} />
+          ) : (
+            <p>No tickets found</p>
+          )
+        }
+
       </div>
     </div>
   );
