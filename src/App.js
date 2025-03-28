cafeabcbb72b6042443082bfdec521ac6b0d43ed
SmpManeth <smpmaneth@gmail.com>
2025-03-28 17:26:48 +0530

    update 2 files and create 1 file

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import TicketForm from "./component/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
 import { useReducer } from "react";
 import TicketList from "./component/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
-  const initialState = { tickets: [] };
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
   const [state, dispatch] = useReducer(ticketReducer, initialState);
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
@@ -18,8 +25,14 @@ function App() {
 
         <div className="result ticket-list">
           <h2>All Tickets</h2>
+
+          <select value={state.sortPreference} onChange={e => dispatch({type:"SET_SORTING" , payload:e.target.value})}>
+            <option value="High to Low">High to Low</option>
+            <option value="Low to High">Low to High</option>
+          </select>
+
           {state.tickets.length > 0 ? (
-            <TicketList tickets={state.tickets} dispatch={dispatch} />
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
           ) : (
             <p>No tickets found</p>
           )}
